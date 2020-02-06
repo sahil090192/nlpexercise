2f860014408f75b4087319b1f98f9d409cfbd709
sahil <sahil090192@gmail.com>
2020-02-06 07:03:55 -0500

    time class work draft 1 WIP

---
+++ timeclas.py
@@ -0,0 +1,19 @@
+
+class TimeSeries:
+
+    def __init__(self, data, frequency, start_date):
+        self.data = data
+        self.frequency = frequency
+        self.start_date - start_date 
+
+    def getData(self):
+        return self.data
+
+    def getFreq(self):
+        return self.frequency
+        
+    def getStartDate(self):
+        return self.start_date
+
+    
+    
